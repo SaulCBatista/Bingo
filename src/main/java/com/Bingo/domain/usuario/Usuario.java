ed697bf40ea460623346072fe86046b5b0f8ce05
Saul C Batista <saulcbatista@gmail.com>
2024-02-25 07:24:26 -0300

    adding user class

--- src/main/java/com/Bingo/domain/usuario/Usuario.java
+++ src/main/java/com/Bingo/domain/usuario/Usuario.java
@@ -1,5 +1,26 @@
 package com.Bingo.domain.usuario;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity(name = "Users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Usuario {
 
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
 }
